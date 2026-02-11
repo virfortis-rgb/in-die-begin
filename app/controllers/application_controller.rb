3de38e63692def8cdb347c03e73611fa2d21fd64
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-11 10:59:37 +0900

    authorize before everyaction in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :autenticate_user!
+  before_action :authenticate_user!
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
