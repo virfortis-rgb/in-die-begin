44f59e44553544f914b9bf66b26a26b06a131288
Damien Joubert <damienusvirfortis@gmail.com>
2026-04-04 11:10:04 +0900

    amove scraping logic to model

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,18 +6,7 @@ class StoriesController < ApplicationController
 
   def show
     @story = Story.find(params[:id])
-    @vocabs = vocabs_from_story
+    @vocabs = @story.create_vocabs_array
   end
 
-  private
-
-
-  def vocabs_from_story
-    vocabs = []
-    @story.content.split() do |c|
-      # word = Word.create(name: c, meaning: c)
-      # vocabs << Vocab.create(rating: 0, story_id: @story.id, word_id: word.id)
-    end
-    return vocabs
-  end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,34 +1,27 @@
 class Story < ApplicationRecord
-  def add_vocab(word)
-    Vocab.new(rating: 0, story_id: self.id, word_id: word.id)
-  end
 
-  def create_vocab()
-    # takes a story and returns an array of vocabs
-    #story.split
-      # get definition of each vocab sand Word.save
-    # create Vocab
-    # push into array
+  def create_vocabs_array
     vocabs = []
     self.content.split() do |c|
-      word = Word.create_or_find_by!(name: c, meaning: scrape_word_definitions)
-      # vocabs << Vocab.create(rating: 0, story_id: @story.id, word_id: word.id)
+      c = "’n" ? c = "'n" : c
+      word = Word.find_or_create_by!(name: c, definitions: scrape_word_definitions(c))
+      vocabs << Vocab.find_or_create_by!(rating: 0, story_id: self.id, word_id: word.id)
     end
     return vocabs
   end
 
   private
 
-  def scrape_word_definitions(word)
-    # return an hash of defs
-    word = word
-    url = ""
-
+  def scrape_word_definitions(afrikaans_word)
+    definitions = {
+      glosbe: []
+    }
+    url = "https://en.glosbe.com/af/en/#{afrikaans_word}"
     html_file = URI.parse(url).read
     html_doc = Nokogiri::HTML.parse(html_file)
-
-    html_doc.search().each do |element|
-      element.text.strip
-    end
+    array = html_doc.xpath("//h3").collect(&:text)
+    definitions[:glosbe] = array.map { |w| w.gsub(/\n/, "") }
+    return definitions
   end
+
 end
