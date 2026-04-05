9931a1df2efe81596100cc3dc6af13f40df07a50
Damien Joubert <damienusvirfortis@gmail.com>
2026-04-05 18:14:17 +0900

    add TODO comments in Story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,14 +1,14 @@
 class Story < ApplicationRecord
   has_many :vocabs, dependent: :destroy
 
-  def create_vocabs
+  def create_vocabs # Vocabs should be properly saved, otherwise can't display NB!!
     puts "Fetching vocabs from glosbe. Wait a moment or two..."
     vocabs = []
     count = 1
     self.content.split() do |c|
       puts "Fetching vocab #{count}/#{self.content.size} for this story.\r"
       c == "’n" ? c = "'n" : c
-      c.include?("." || ",") ? c = c.gsub("." || ",", "") : c
+      c.include?("." || ",") ? c = c.gsub("." || ",", "") : c  # TODO all punctuation
       word = Word.find_or_create_by!(name: c, definitions: scrape_word_definitions(c))
       vocabs << Vocab.find_or_create_by!(rating: 0, story_id: self.id, word_id: word.id)
       n = 1
